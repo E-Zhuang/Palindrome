ffe2be5b3dadfeb429551f604a69e48677dac132
E-Zhuang <springreading@gmail.com>
2016-10-12 16:27:39 -0700

    Updates the findPal( ) method and adds comments

--- Palindrome.java
+++ Palindrome.java
@@ -1,6 +1,5 @@
 
 /**
- * 
  * Emily Zhuang
  * APCS:Period 4
  * Mr. Lantsberger
@@ -9,98 +8,74 @@
 
 public class Palindrome
 {
-    String lowerPal, output;   
-    int length, character, back, punct;
-        
+    private String lowerPal;   
+    private int length;
     
+    /**
+     * This constructor method contains the lowercase version of the String pal and also the 
+     * initial length of the string.
+     */
     public Palindrome(String pal)
     {
         lowerPal = pal.toLowerCase();
         length = lowerPal.length(); 
-        character = 0;
-        back = 1;
     }
     
+    /**
+     * This modifier method will first test to see if a string is a single character. If it is a single
+     * character, the method returns that the String is not a palindrome. If it is more than one character,
+     * the method will then remove all punctuation using a nested for and while loop. The for loops determines
+     * which punctuation mark to test for while the while loops removes the character through substrings
+     * and concatenation. The method then compares the first and last character in the string to see if 
+     * they are the same and at the end of the while loop the method tests to see if all the characters
+     * were tested through an if-else statement. 
+     */
     public String testPal()
     {
+        String output, allPunct, test;
+        int character, back, punct;
+        
+        character = 0;
+        back = 1;
+        
+        
         if (length == 1) 
             output = "No, the string you entered is NOT a palindrome. ";
             
         else
         {
             // remove spaces and punctuation
-            punct = lowerPal.indexOf(" ");
-            while (punct != -1)
+            allPunct = " ,.!?;:-";
+            
+            for (int many = 0; many < allPunct.length(); many++)
             {
-                length = lowerPal.length(); 
-                lowerPal = lowerPal.substring (0, punct) + lowerPal.substring ((punct + 1), (length));
-                punct = lowerPal.indexOf(" ");
+                test = "";
+                test += allPunct.charAt(many);
+               
+            
+                punct = lowerPal.indexOf(test);
+                while (punct != -1)
+                {
+                    length = lowerPal.length(); 
+                    lowerPal = lowerPal.substring (0, punct) + lowerPal.substring ((punct + 1), (length));
+                    punct = lowerPal.indexOf(test);
+                }
             }
-            punct = lowerPal.indexOf(",");
-            while (punct != -1)
-            {
-                length = lowerPal.length(); 
-                lowerPal = lowerPal.substring (0, punct) + lowerPal.substring ((punct + 1), (length));
-                punct = lowerPal.indexOf(",");
-            }
-            punct = lowerPal.indexOf(".");
-            while (punct != -1)
-            {
-                length = lowerPal.length(); 
-                lowerPal = lowerPal.substring (0, punct) + lowerPal.substring ((punct + 1), (length));
-                punct = lowerPal.indexOf(".");
-            }
-            punct = lowerPal.indexOf("!");
-            while (punct != -1)
-            {
-                length = lowerPal.length(); 
-                lowerPal = lowerPal.substring (0, punct) + lowerPal.substring ((punct + 1), (length));
-                punct = lowerPal.indexOf("!");
-            }
-            punct = lowerPal.indexOf("?");
-            while (punct != -1)
-            {
-                length = lowerPal.length(); 
-                lowerPal = lowerPal.substring (0, punct) + lowerPal.substring ((punct + 1), (length));
-                punct = lowerPal.indexOf("?");
-            }
-            punct = lowerPal.indexOf(";");
-            while (punct != -1)
-            {
-                length = lowerPal.length(); 
-                lowerPal = lowerPal.substring (0, punct) + lowerPal.substring ((punct + 1), (length));
-                punct = lowerPal.indexOf(";");
-            }
-            punct = lowerPal.indexOf(":");
-            while (punct != -1)
-            {
-                length = lowerPal.length(); 
-                lowerPal = lowerPal.substring (0, punct) + lowerPal.substring ((punct + 1), (length));
-                punct = lowerPal.indexOf(":");
-            }
-            punct = lowerPal.indexOf("-");
-            while (punct != -1)
-            {
-                length = lowerPal.length(); 
-                lowerPal = lowerPal.substring (0, punct) + lowerPal.substring ((punct + 1), (length));
-                punct = lowerPal.indexOf("-");
-            }
-                
+
             length = lowerPal.length(); 
                 
             //Compare the first char to the last char and so on.
-            while ((character < ((double) length / 2)) && (lowerPal.charAt(character) == lowerPal.charAt(length - back)))
+            while ((character < (length / 2)) && (lowerPal.charAt(character) == lowerPal.charAt(length - back)))
             {
                 character++;
                 back++; 
             }
                 
-            if ((character - 1) == (length / 2))
+            if ((length > 1) && (character == (length / 2)))
                 output = "Yes, the string you entered is a palindrome. ";
             else
                 output = "No, the string you entered is NOT a palindrome. ";
             }
-
             
         return output;
 
--- PalindromeTester.java
+++ PalindromeTester.java
@@ -1,37 +1,54 @@
 
 /**
- * Write a description of class PalindromeTester here.
+ * This code tests to see if a line of text is a palindrome or not.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * Emily Zhuang
+ * APCS:Period 4
+ * Mr. Lantsberger
+ * 12 October 2016
  */
 
 import chn.util.*;
 
 public class PalindromeTester
 {
+    /**
+     * This method will continuously prompt the user for a string and test whether the string
+     * is a palindrome or not using a while loop. If the user inputs "Q" or "q" the loop will
+     * not reiterate and the code will end. The method called is testPal() which is defined in
+     * the Palindrome class. 
+     */
+    
     public static void main(String[] args)
     {
+        //Declares object refrences
         ConsoleIO keyboard = new ConsoleIO();
         Palindrome machine;
         
+        //Declares primatives 
         String pal, lowerPal, output;  
         
+        //Introduces the program
         System.out.println("Welcome to the Palindrome Program!");
         System.out.println();
         
+        //Prompts the user for a string
         System.out.print("Enter a string (Q or q to quit): ");
         pal= keyboard.readLine();
         
+        //Continues to test if a string is a palindrome and prompts the user for another line
         while (!pal.equals("Q") && !pal.equals("q")) 
         {
+            //instantiates Palindrome object
             machine = new Palindrome(pal);
-                
+            
+            //calls method to test the Palindrome and prints whether it is a Palindrome or not
             output = machine.testPal();
             System.out.println(output);
             System.out.println();
             
-            System.out.print("Enter a string (Q or q to quit: ");
+            //reprompts the user
+            System.out.print("Enter a string (Q or q to quit): ");
             pal= keyboard.readLine();
         
         }
